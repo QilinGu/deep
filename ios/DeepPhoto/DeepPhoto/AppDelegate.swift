d274a3ad88b32da168266806d335a0da3a7f6bac
Daniel Velasco <velasc.daniel@gmail.com>
2016-11-15 12:29:52 -0700

    Added an xCode project

---
+++ ios/DeepPhoto/DeepPhoto/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeepPhoto
+//
+//  Created by Daniel Velasco on 2016-11-15.
+//  Copyright © 2016 Deep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ios/DeepPhoto/DeepPhoto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeepPhoto
+//
+//  Created by Daniel Velasco on 2016-11-15.
+//  Copyright © 2016 Deep. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
