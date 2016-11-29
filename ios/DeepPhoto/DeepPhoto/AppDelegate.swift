e7b58676749c51db024d65e9660669b4831474e2
danielVelasc <velasc.daniel@gmail.com>
2016-11-29 12:04:46 -0700

    Have pasted code from TensorFlow's iOS repository

--- ios/DeepPhoto/DeepPhoto/AppDelegate.swift
+++ ios/DeepPhoto/DeepPhoto/AppDelegate.swift
@@ -7,20 +7,37 @@
 //
 
 import Cocoa
-
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+import "CameraExampleAppDelegate.h"
+    
+    @implementation CameraExampleAppDelegate
+    
+    @synthesize window = _window;
+    
+    - (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self.window makeKeyAndVisible];
+    return YES;
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    - (void)applicationWillResignActive:(UIApplication *)application {
+    [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
     }
-
-
+    
+    - (void)applicationDidEnterBackground:(UIApplication *)application {
+    }
+    
+    - (void)applicationWillEnterForeground:(UIApplication *)application {
+    }
+    
+    - (void)applicationDidBecomeActive:(UIApplication *)application {
+    [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
+    }
+    
+    - (void)applicationWillTerminate:(UIApplication *)application {
+    }
+    
+    end
+    
+    
 }
 
